cbe6cebd3b8c6a23e302752fd1e90de7044d33cd
dallas marlow <dallasmarlow@gmail.com>
2012-05-18 15:20:50 -0400

    latency

--- lib/measureable.rb
+++ lib/measureable.rb
@@ -1,3 +1,13 @@
 # measureable
 $:.unshift File.join File.dirname(__FILE__), 'measureable'
 require 'counters'
+
+module Measureable
+
+  def self.latency counter
+    checkpoint = Time.now
+    yield
+    counter.increment Time.now - checkpoint
+  end
+
+end
